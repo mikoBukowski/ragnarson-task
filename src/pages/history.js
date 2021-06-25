14f653ac2c0d69df3cb337782ee7519bf2d737fb
miko <mikolaj.bukowski9@gmail.com>
2021-06-25 18:27:33 +0200

    Added barebones layout

---
+++ src/pages/history.js
@@ -0,0 +1,80 @@
+import { Container } from '../components/Container'
+
+import { 
+  Flex, 
+  Heading,
+  VStack,
+  Button,
+} from '@chakra-ui/react'
+
+import Link from 'next/link'
+
+const History = () => (
+  <>
+  {/* <Container>
+    <VStack
+      border="1px"
+      h={['88%', '68%']}
+      w={['88%', '68%']}
+      // justifyContent="space-between"
+      bgGradient="linear(to-tr, teal.300,blue.400)" 
+    >
+      <Flex
+        h="8vh"
+        w="100%"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Link href="/">
+          <Button>Back to game</Button>
+        </Link>
+      </Flex>
+
+      <Heading
+        fontWeight={700}
+        fontSize={{ base: '4xl', md: '6xl' }}
+        textShadow="rgb(0 0 0 /12%) 0 6px 16px"        
+      >
+        Results grid 
+      </Heading>
+
+
+
+    </VStack>
+  </Container> */}
+
+  <Container>
+    <VStack
+      h={['90%', '68%']}
+      w={['90%', '68%']}
+      borderRadius={10}
+      boxShadow="rgb(0 0 0 /12%) 0 6px 16px"
+      justifyContent="start"
+      bgGradient="linear(to-tr, teal.300,blue.400)" 
+    >
+      <Flex
+        h="8vh"
+        w="100%"
+        m={5}
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Link href="/">
+          <Button>Back to game</Button>
+        </Link>
+      </Flex>
+      
+      <Heading
+        fontWeight={700}
+        fontSize={{ base: '4xl', md: '6xl' }}
+        textShadow="rgb(0 0 0 /12%) 0 6px 16px"        
+      >
+        Game Results
+      </Heading>
+
+    </VStack>
+  </Container>
+  </>
+)
+
+export default History
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,78 @@
 import { Container } from '../components/Container'
+
 import { 
   Flex, 
+  Heading,
+  VStack,
+  Image,
+  Spacer,
+  Button,
   Center 
 } from '@chakra-ui/react'
+
+import Link from 'next/link'
 
 const Index = () => (
   <>
   <Container>
-    <Center
-      border="1px"
-      h="78%"
-      w="78%"
+    <VStack
+      h={['90%', '68%']}
+      w={['90%', '68%']}
+      borderRadius={10}
+      boxShadow="rgb(0 0 0 /12%) 0 6px 16px"
+      justifyContent="space-around"
+      bgGradient="linear(to-tr, teal.300,blue.400)" 
     >
-      dupa
-    </Center>
+      <Flex
+        h="8vh"
+        w="100%"
+        alignItems="center"
+        justifyContent="space-around"
+        // border="1px"
+      >
+        <Flex>Points</Flex>
+        <Link href="/history">
+          <Button>History</Button>
+        </Link>
+        <Flex>Round</Flex>
+      </Flex>
+
+
+      <Image
+        h={200}
+        w={200}
+        src="/logo-ams.png"
+        border="1px"
+      />
+      
+      <Heading
+        fontWeight={700}
+        fontSize={{ base: '4xl', md: '6xl' }}
+        textShadow="rgb(0 0 0 /12%) 0 6px 16px"        
+        
+      >
+        Roll the dice!
+      </Heading>
+
+      <Flex
+        h="18%"
+        w="100%"
+        // alignItems="center"
+        justifyContent="space-evenly"
+        // border="1px"
+      >
+        <Button
+          p={"9"}
+        >
+          Higher</Button>
+
+        <Button
+          p={"9"}
+        >
+          Lower</Button>  
+      </Flex>
+
+    </VStack>
   </Container>
   </>
 )
