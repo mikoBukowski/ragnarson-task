9ddbe9a33bc70a2b94341547b41eda89ea7d4ebe
miko <mikolaj.bukowski9@gmail.com>
2021-07-07 14:58:46 +0200

    semi working local storage implementation - to be tested

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import {
   Heading,
   VStack,
   Image,
+  IconButton,
   Button,
 } from "@chakra-ui/react"
 import Link from "next/link"
@@ -16,11 +17,13 @@ const Index = () => {
   const [userChoice, setUserChoice] = useState(null)
   const [score, setScore] = useState(0)
   const [round, setRound] = useState(0)
-  const [gameResults, setGameResults] = useState([0])
   const [isError, setIsError] = useState(false)
+  const [display, changeDisplay] = useState('none')
+  const [resume, setResume] = useState('none')
+  const [gameResults, setGameResults] = useState([])
   const endpoint = "https://dice-api.genzouw.com/v1/dice"
   // const endpoint = "http://roll.diceapi.com/json/d6"
-  // changed to this specific endpoint due to cors policy, HTTPS does the trick // .then(payload => data.push(payload.dice[0].value))  
+  // changed to this specific endpoint due to cors policy during deployment, HTTPS does the trick // .then(payload => data.push(payload.dice[0].value))  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,24 +37,35 @@ const Index = () => {
         resetScore()
         window.localStorage.setItem('round', round)
         window.localStorage.setItem('score', score)
-        setGameResults()
       } 
       catch (error) {
         setIsError(true)
       }
     }
     fetchData()
+
   }, [round])
 
   useEffect(() => {
     setRound(JSON.parse(window.localStorage.getItem('round')))
     setScore(JSON.parse(window.localStorage.getItem('score')))
+    handlePageReload()
   }, [])
   
+  const handlePageReload = () => {
+    if (round !== 0) {
+      if (window.performance) {
+        if (performance.navigation.type == 1) {
+          setResume('flex')
+        } 
+      }  
+    }
+  }
+
   const checkResults = (currentDice) => {
-    console.log(`'CURRENT DICE - ${currentDice}'`)
-    console.log(`'PREVIOUS DICE - ${dice}'`)
-    console.log(`'USERS CHOICE - ${userChoice}'`)
+    // console.log(`'CURRENT DICE - ${currentDice}'`)
+    // console.log(`'PREVIOUS DICE - ${dice}'`)
+    // console.log(`'USERS CHOICE - ${userChoice}'`)
 
     if (userChoice) {
         dice <= currentDice ? 
@@ -95,7 +109,6 @@ const Index = () => {
             justifyContent="space-evenly"
           >
             <Flex>Score {score.toFixed(1)}</Flex>
-            <Link href="/history">
               <Button
                 p={"9"}
                 borderRadius="full"
@@ -103,10 +116,10 @@ const Index = () => {
                 boxShadow="rgb(0 0 0 /12%) 0 6px 16px"
                 fontWeight={700}
                 fontSize={"xl"}
+                onClick={() => changeDisplay('flex')}
               >
                 Game <br></br> Results
               </Button>
-            </Link>
             <Flex>Round {round}</Flex>
           </Flex>
           
@@ -157,8 +170,110 @@ const Index = () => {
             >
               Lower
             </Button>
-          </Flex>
+          </Flex>  
         </VStack>
+      
+        {/* Game results */}
+
+        <VStack
+          h={['88%', '80%', '75%', '68%']}
+          w={['99%', '80%', '75%', '50%']}
+          borderRadius={10}
+          boxShadow="dark-lg"
+          justifyContent="start"
+          bgGradient="linear(to-tr, teal.300,blue.400)" 
+          pos="fixed"
+          overflowY="auto"
+          flexDir="column"
+          display={display}
+        >
+          <Flex
+            h="12vh"
+            w="100%"
+            m={6}
+            alignItems="center"
+            justifyContent="space-evenly"
+          >
+              <Button
+                p={"9"}
+                borderRadius="full"
+                bgGradient="linear(to-tr, teal.300,yellow.400)"
+                boxShadow="rgb(0 0 0 /12%) 0 6px 16px"
+                fontWeight={700}
+                fontSize={"xl"}
+                onClick={() => changeDisplay('none')}
+              >
+                Back <br></br> to game
+              </Button>
+          </Flex>
+          
+          <Heading
+            fontWeight={700}
+            fontSize={{ base: '5xl', md: '8xl' }}
+            textShadow="rgb(0 0 0 /12%) 0 6px 16px"        
+          >
+            Game Results
+          </Heading>
+        </VStack>
+        
+        {/* Resume game? */}
+        <VStack
+          h={['88%', '80%', '75%', '68%']}
+          w={['99%', '80%', '75%', '50%']}
+          borderRadius={10}
+          boxShadow="dark-lg"
+          justifyContent="start"
+          bgGradient="linear(to-tr, teal.300,blue.400)" 
+          pos="fixed"
+          overflowY="auto"
+          flexDir="column"
+          display={resume}
+        >
+
+          <Heading
+            fontWeight={700}
+            fontSize={{ base: '5xl', md: '8xl' }}
+            textShadow="rgb(0 0 0 /12%) 0 6px 16px"        
+            m={8}
+          >
+            Resume previous game? 
+          </Heading>
+
+          <Flex
+            h="100vh"
+            w="100%"
+            alignItems="center"
+            // direction='column'
+            justifyContent="space-evenly"
+          >
+              
+              <Button
+                p={"9"}
+                borderRadius="full"
+                bgGradient="linear(to-tr, teal.300,yellow.400)"
+                boxShadow="rgb(0 0 0 /12%) 0 6px 16px"
+                fontWeight={700}
+                fontSize={"xl"}
+                onClick={() => setResume('none')}
+              >
+                Sure
+              </Button>
+
+              <Button
+                p={"9"}
+                borderRadius="full"
+                bgGradient="linear(to-tr, teal.300,yellow.400)"
+                boxShadow="rgb(0 0 0 /12%) 0 6px 16px"
+                fontWeight={700}
+                fontSize={"xl"}
+                onClick={() => { resetScore(); setResume('none'); }}
+                >
+                No
+              </Button>
+          </Flex>
+          
+        </VStack>
+
       </Container>
     </>
   );
